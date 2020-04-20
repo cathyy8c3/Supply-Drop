8858512ca7f69e1d37f8bfa4cf1f6ad4d05482c5
cathyy8c3 <cathyy8c3@gmail.com>
2020-04-20 13:14:28 -0700

    made request codable

--- Supply Drop/Templates/Classes/Order.swift
+++ Supply Drop/Templates/Classes/Order.swift
@@ -10,17 +10,69 @@ import Foundation
 import SwiftUI
 import Combine
 
-class Request:ObservableObject{
+class Request:ObservableObject,Codable,Identifiable{
     @Published var org_name: String = ""
     @Published var item: String = ""
     @Published var num: Int = 0
     @Published var date:String = ""
     
     @Published var address:Address = Address()
+    @Published var addressString:String = ""
+    
+    @Published var requesterID:String = ""
+    @Published var donorID:String = ""
     
     @Published var requester:User = User()
     @Published var donor:User = User()
     @Published var claimed:Bool = false
+    
+    var id = UUID().uuidString
+    
+    init(){}
+    
+    //0 = not sent, 1 = sent, 2 = received
+    @Published var status:Int = 0
+    @Published var complete:Bool = false
+    
+    enum CodingKeys: String, CodingKey {
+        case org_name = "Title"
+        case item = "Item"
+        case num = "AffiliateLinks"
+        case addressString = "ShippingAddress"
+        case date = "Description"
+        case id = "ID"
+        case status = "State"
+        case requesterID = "Requester"
+        case donorID = "Donor"
+    }
+    
+    required init(from decoder:Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        org_name = try values.decode(String.self, forKey: .org_name)
+        item = try values.decode(String.self, forKey: .item)
+        num = try values.decode(Int.self, forKey: .num)
+        addressString = try values.decode(String.self, forKey: .addressString)
+        date = try values.decode(String.self, forKey: .date)
+        id = try values.decode(String.self, forKey: .id)
+        status = try values.decode(Int.self, forKey: .status)
+        requesterID = try values.decode(String.self, forKey: .requesterID)
+        donorID = try values.decode(String.self, forKey: .donorID)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(org_name, forKey: .org_name)
+        try container.encode(item, forKey: .item)
+        try container.encode(num, forKey: .num)
+        try container.encode(addressString, forKey: .addressString)
+        try container.encode(date, forKey: .date)
+        try container.encode(id, forKey: .id)
+        try container.encode(status, forKey: .status)
+        try container.encode(requesterID, forKey: .requesterID)
+        try container.encode(donorID, forKey: .donorID)
+    }
     
     func setOrder2Order(order2:Request){
         org_name = order2.org_name
@@ -37,13 +89,6 @@ class Request:ObservableObject{
     func getItem()->String{
         return item
     }
-    
-    //todo
-    let id = UUID().uuidString
-    
-    //0 = not sent, 1 = sent, 2 = received
-    @Published var status:Int = 0
-    @Published var complete:Bool = false
     
     func valid(n:Int)->Bool{
         if(item.count==0 || n==0 || !(date.count==10)){
@@ -68,12 +113,14 @@ class Request:ObservableObject{
         //todo
         
         requester=u
+        requesterID = u.id
     }
     
     func setDonor(u:User){
         //todo
         
         donor=u
+        donorID = u.id
     }
     
     func status2string()->String{
@@ -105,3 +152,33 @@ class Request:ObservableObject{
     }
 }
 
+extension Api{
+    func getOrder(completion: @escaping(Request,Bool) -> ()){
+        guard let url = URL(string: "https://reqres.in/api/") else{
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if(error==nil && !(data==nil)){
+                do{
+                    
+                    let order1 = try!JSONDecoder().decode(Request.self, from: data!)
+                    
+                    if(type(of:data)==String.self){
+                        completion(order1,false)
+                    }
+                    
+                    completion(order1,true)
+                }
+                
+                catch{
+                    print("Error in JSON parsing.")
+                }
+            }else{
+                print("Error")
+                return
+            }
+        }.resume()
+    }
+}
